f83381a8779d1568c2a01e2ff2fd206dbdc708c2
Matt Metcalf <mrm9084@gmail.com>
2023-10-19 10:40:48 -0700

    Update __init__.py

--- microsoft/featuremanagement/__init__.py
+++ microsoft/featuremanagement/__init__.py
@@ -1,1 +1,13 @@
-__path__ = __import__("pkgutil").extend_path(__path__, __name__)  # type: ignore
+# ------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# -------------------------------------------------------------------------
+from ._featuremanager import FeatureManager
+from ._featurefilters import FeatureFilter
+from ._defaultfilters import TimeWindowFilter, TargetingFilter
+
+from ._version import VERSION
+
+__version__ = VERSION
+__all__ = ["FeatureManager", "TimeWindowFilter", "TargetingFilter", "FeatureFilter"]
