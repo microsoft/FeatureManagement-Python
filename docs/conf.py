2aca77e25fb5ccc7e1d3d3d0cece712dc4d96e75
Matt Metcalf <mrm9084@gmail.com>
2024-02-22 14:41:17 -0800

    formatting

--- docs/conf.py
+++ docs/conf.py
@@ -4,19 +4,20 @@
 import os
 import sys
 import sphinx_rtd_theme
+
 sys.path.insert(0, os.path.abspath("../microsoft"))
 
-project = 'FeatureManagement Python'
-copyright = '2024, Microsoft'
-author = 'Microsoft'
-release = '1.0.0b1'
+project = "FeatureManagement Python"
+copyright = "2024, Microsoft"
+author = "Microsoft"
+release = "1.0.0b1"
 
 # -- General configuration ---------------------------------------------------
 
-extensions = ['myst_parser', 'sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon']
+extensions = ["myst_parser", "sphinx.ext.autodoc", "sphinx.ext.coverage", "sphinx.ext.napoleon"]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 autodoc_default_options = {
     "members": True,
@@ -24,11 +25,11 @@ autodoc_default_options = {
 }
 
 autosummary_mock_imports = [
-    'microsoft.featuremanagement._models',
+    "microsoft.featuremanagement._models",
 ]
 
 # -- Options for HTML output -------------------------------------------------\
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-html_static_path = ['_static']
+html_static_path = ["_static"]
