ae94feca364e10486a3d6279f58fc1cd8c805c8e
Matt Metcalf <mrm9084@gmail.com>
2024-03-27 15:01:07 -0700

    Updating with PyLint

--- tests/validation_tests/test_json_validations.py
+++ tests/validation_tests/test_json_validations.py
@@ -3,21 +3,22 @@
 # Licensed under the MIT License. See License.txt in the project root for
 # license information.
 # --------------------------------------------------------------------------
-from tests.validation_tests.load_feature_flags import LoadFeatureFlagsFromFile
+
 import unittest
 import json
+from featuremanagement import FeatureManager
 
-file_path = "tests/validation_tests/"
-sample_json_key = ".sample.json"
-tests_json_key = ".tests.json"
-test_id_key = "TestID"
-expected_result_key = "ExpectedResult"
-feature_flag_name_key = "FeatureFlagName"
-inputs_key = "Inputs"
-description_key = "Description"
+FILE_PATH = "tests/validation_tests/"
+SAMPLE_JSON_KEY = ".sample.json"
+TESTS_JSON_KEY = ".tests.json"
+TEST_ID_KEY = "TestID"
+EXPECTED_RESULT_KEY = "ExpectedResult"
+FEATURE_FLAG_NAME_KEY = "FeatureFlagName"
+INPUTS_KEY = "Inputs"
+DESCRIPTION_KEY = "Description"
 
 
-class TestNoFiltersFromFile(LoadFeatureFlagsFromFile, unittest.TestCase):
+class TestNoFiltersFromFile(unittest.TestCase):
     # method: is_enabled
     def test_no_filters(self):
         test_key = "NoFilters"
@@ -43,31 +44,42 @@ class TestNoFiltersFromFile(LoadFeatureFlagsFromFile, unittest.TestCase):
         test_key = "RequirementType"
         self.run_tests(test_key)
 
+    @staticmethod
+    def load_from_file(file):
+        with open("tests/validation_tests/" + file, "r", encoding="utf-8") as feature_flags_file:
+            feature_flags = json.load(feature_flags_file)
+
+        feature_manager = FeatureManager(feature_flags)
+        assert feature_manager is not None
+
+        return feature_manager
+
+
     # method: is_enabled
     def run_tests(self, test_key):
-        feature_manager = self.load_from_file(test_key + sample_json_key)
+        feature_manager = self.load_from_file(test_key + SAMPLE_JSON_KEY)
 
-        feature_flag_test_file = open(file_path + test_key + tests_json_key, "r")
-        feature_flag_tests = json.load(feature_flag_test_file)
+        with open(FILE_PATH + test_key + TESTS_JSON_KEY, "r", encoding="utf-8") as feature_flag_test_file:
+            feature_flag_tests = json.load(feature_flag_test_file)
 
         for feature_flag_test in feature_flag_tests:
-            expected_result = feature_flag_test[expected_result_key]
+            expected_result = feature_flag_test[EXPECTED_RESULT_KEY]
             failed_description = (
-                "Test " + feature_flag_test[test_id_key] + " failed. Description: " + feature_flag_test[description_key]
+                "Test " + feature_flag_test[TEST_ID_KEY] + " failed. Description: " + feature_flag_test[DESCRIPTION_KEY]
             )
 
             if isinstance(expected_result, bool):
-                if inputs_key in feature_flag_test:
-                    user = feature_flag_test[inputs_key].get("user", None)
-                    groups = feature_flag_test[inputs_key].get("groups", [])
+                if INPUTS_KEY in feature_flag_test:
+                    user = feature_flag_test[INPUTS_KEY].get("user", None)
+                    groups = feature_flag_test[INPUTS_KEY].get("groups", [])
                     assert (
-                        feature_manager.is_enabled(feature_flag_test[feature_flag_name_key], user=user, groups=groups)
+                        feature_manager.is_enabled(feature_flag_test[FEATURE_FLAG_NAME_KEY], user=user, groups=groups)
                         == expected_result
                     ), failed_description
                 else:
                     assert (
-                        feature_manager.is_enabled(feature_flag_test[feature_flag_name_key]) == expected_result
+                        feature_manager.is_enabled(feature_flag_test[FEATURE_FLAG_NAME_KEY]) == expected_result
                     ), failed_description
             else:
                 with self.assertRaises(ValueError):
-                    feature_manager.is_enabled(feature_flag_test[feature_flag_name_key]), failed_description
+                    feature_manager.is_enabled(feature_flag_test[FEATURE_FLAG_NAME_KEY])
