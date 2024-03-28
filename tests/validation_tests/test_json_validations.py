4f52578f6ba93b45e7275e2c990c855adbd108aa
Matt Metcalf <mrm9084@gmail.com>
2024-03-28 10:09:46 -0700

    Updated to match AppConfiguration repo

--- tests/validation_tests/test_json_validations.py
+++ tests/validation_tests/test_json_validations.py
@@ -4,8 +4,9 @@
 # license information.
 # --------------------------------------------------------------------------
 
-import unittest
 import json
+import unittest
+from pytest import raises
 from featuremanagement import FeatureManager
 
 FILE_PATH = "tests/validation_tests/"
@@ -13,9 +14,24 @@ SAMPLE_JSON_KEY = ".sample.json"
 TESTS_JSON_KEY = ".tests.json"
 TEST_ID_KEY = "TestID"
 EXPECTED_RESULT_KEY = "ExpectedResult"
+IS_ENABLED_KEY = "IsEnabled"
 FEATURE_FLAG_NAME_KEY = "FeatureFlagName"
 INPUTS_KEY = "Inputs"
+USER_KEY = "user"
+GROUPS_KEY = "groups"
+EXCEPTION_KEY = "Exception"
+EXCEPTION_MESSAGE_KEY = "Message"
 DESCRIPTION_KEY = "Description"
+
+
+def convert_boolean_value(enabled):
+    if isinstance(enabled, bool):
+        return enabled
+    if enabled.lower() == "true":
+        return True
+    if enabled.lower() == "false":
+        return False
+    return enabled
 
 
 class TestNoFiltersFromFile(unittest.TestCase):
@@ -46,7 +62,7 @@ class TestNoFiltersFromFile(unittest.TestCase):
 
     @staticmethod
     def load_from_file(file):
-        with open("tests/validation_tests/" + file, "r", encoding="utf-8") as feature_flags_file:
+        with open(FILE_PATH + file, "r", encoding="utf-8") as feature_flags_file:
             feature_flags = json.load(feature_flags_file)
 
         feature_manager = FeatureManager(feature_flags)
@@ -63,22 +79,20 @@ class TestNoFiltersFromFile(unittest.TestCase):
 
         for feature_flag_test in feature_flag_tests:
             expected_result = feature_flag_test[EXPECTED_RESULT_KEY]
+            expected_is_enabled_result = convert_boolean_value(expected_result.get(IS_ENABLED_KEY))
             failed_description = (
                 "Test " + feature_flag_test[TEST_ID_KEY] + " failed. Description: " + feature_flag_test[DESCRIPTION_KEY]
             )
 
-            if isinstance(expected_result, bool):
-                if INPUTS_KEY in feature_flag_test:
-                    user = feature_flag_test[INPUTS_KEY].get("user", None)
-                    groups = feature_flag_test[INPUTS_KEY].get("groups", [])
-                    assert (
-                        feature_manager.is_enabled(feature_flag_test[FEATURE_FLAG_NAME_KEY], user=user, groups=groups)
-                        == expected_result
-                    ), failed_description
-                else:
-                    assert (
-                        feature_manager.is_enabled(feature_flag_test[FEATURE_FLAG_NAME_KEY]) == expected_result
-                    ), failed_description
+            if isinstance(expected_is_enabled_result, bool):
+                user = feature_flag_test[INPUTS_KEY].get(USER_KEY, None)
+                groups = feature_flag_test[INPUTS_KEY].get(GROUPS_KEY, [])
+                assert (
+                    feature_manager.is_enabled(feature_flag_test[FEATURE_FLAG_NAME_KEY], user=user, groups=groups)
+                    == expected_is_enabled_result
+                ), failed_description
             else:
-                with self.assertRaises(ValueError):
+                with raises(ValueError) as ex_info:
                     feature_manager.is_enabled(feature_flag_test[FEATURE_FLAG_NAME_KEY])
+                expected_message = feature_flag_test.get(EXCEPTION_KEY).get(EXCEPTION_MESSAGE_KEY)
+                assert str(ex_info.value) == expected_message, failed_description
