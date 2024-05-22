84bf2424145eb8ce7fe8fa8a3d336c3dbb54b50d
Matt Metcalf <mrm9084@gmail.com>
2024-05-22 13:52:29 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,13 @@ release = "1.0.0b1"
 
 # -- General configuration ---------------------------------------------------
 
-extensions = ["myst_parser", "sphinx.ext.autodoc", "sphinx.ext.coverage", "sphinx.ext.napoleon", "sphinx_toolbox.more_autodoc.autonamedtuple"]
+extensions = [
+    "myst_parser",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.coverage",
+    "sphinx.ext.napoleon",
+    "sphinx_toolbox.more_autodoc.autonamedtuple",
+]
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
