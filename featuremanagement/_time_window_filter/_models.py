9657883ccf8225e963c7fdb02419b533dd2b6eab
Matt Metcalf <mrm9084@gmail.com>
2025-04-18 17:11:18 -0700

    Update _models.py

--- featuremanagement/_time_window_filter/_models.py
+++ featuremanagement/_time_window_filter/_models.py
@@ -105,7 +105,7 @@ class RecurrenceRange:  # pylint: disable=too-few-public-methods
             try:
                 self.end_date = parsedate_to_datetime(end_date_str) if end_date_str else None
             except ValueError as e:
-                raise ValueError(f'Invalid value for EndDate: {end_date_str}') from e
+                raise ValueError(f"Invalid value for EndDate: {end_date_str}") from e
         self.num_of_occurrences = range_data.get("NumberOfOccurrences", math.pow(2, 63) - 1)
         if self.num_of_occurrences < 0:
             raise ValueError("The number of occurrences must be greater than or equal to 0.")
